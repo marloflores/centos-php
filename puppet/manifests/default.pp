80c8ca2cfbfa529fa9b2356a9438b80a83579b94
Marlo Flores <marloflores2000@gmail.com>
2015-12-01 13:18:06 +0100

    ghost configuration to handle php. See puppetlabs-apache for a better way for this module to enable this settings.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -6,11 +6,15 @@ class { 'apache':
 }
 
 class vhost { 
+	$php_fragment = "LoadModule php5_module	modules/libphp5.so\nAddHandler php5-script	.php\n\nDirectoryIndex index.html index.php\nAddType text/html	.php\nAddType application/x-httpd-php-source phps"
+	
 	apache::vhost { 'centos.dev':
-		servername	=> 'centos.dev',
-		port		=> '80',
-		docroot		=> '/vagrant/site',
-#		require		=> Class['apache'],	
+		servername		=> 'centos.dev',
+		port			=> '80',
+		docroot			=> '/vagrant/site',
+		docroot_owner	=> 'apache',
+		docroot_group	=> 'apache',
+		custom_fragment	=> $php_fragment,
 	}
 }
 
