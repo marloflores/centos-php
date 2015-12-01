3e10ca314eff735ab3fbe66ef6c8236e306df00f
Marlo Flores <marloflores2000@gmail.com>
2015-12-01 01:33:57 +0100

    basic vhost settings

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,8 +2,19 @@ class { 'php':
 }
 
 class { 'apache':
+	default_vhost	=> false,
 }
 
+class vhost { 
+	apache::vhost { 'centos.dev':
+		servername	=> 'centos.dev',
+		port		=> '80',
+		docroot		=> '/vagrant/site',
+#		require		=> Class['apache'],	
+	}
+}
+
+include vhost
 include apache
 include php
 
